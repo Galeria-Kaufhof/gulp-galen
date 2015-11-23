7c98825f83fef9a1bd5d80d318eb75c4eebbcc3d
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2015-11-23 10:48:25 +0100

    Restructured tests

--- test/basics.js
+++ test/basics.js
@@ -9,65 +9,69 @@ var rimraf = require("rimraf");
 
 var gulpGalen = require('../index.js');
 
-describe("gulp-galen's code functionality", function () {
+describe("gulp-galen", function () {
 
-  this.timeout(30000);
+  describe("basic functionality", function () {
 
-  var options = {
-     url: "https://www.google.com",
-     size: "800x600",
-     galenPath: "./node_modules/.bin/galen"
-   };
+    this.timeout(30000);
 
-  it("should iterate over some gspecs", function (done) {
-    gulp.src("**/specs/google?.gspec")
-      .pipe(gulpGalen.check(options))
-      .pipe(es.writeArray(function (err, arr) {
-        assert(!err, "There where errors present");
-        assert(arr, "Result missing");
-        assert(arr.length === 2, "False number of files: " + util.inspect(arr));
-        for (var i = 0; i < 2; i++) {
-          assert(arr[i].path.match(/specs\/google.\.gspec$/), "File's path didn't end with specs/google?.gspec: '" + arr[i].path + "'");
-        }
-        done();
-      }));
-  });
+    var options = {
+      url: "https://www.google.com",
+      size: "800x600",
+      galenPath: "./node_modules/.bin/galen"
+    };
 
-  it("should handle failed specs", function (done) {
-    gulp.src("**/specs/google_failing.gspec")
-      .pipe(gulpGalen
-        .check(options)
-        .on("error", function () {
+    it("should iterate over some gspecs", function (done) {
+      gulp.src("**/specs/google?.gspec")
+        .pipe(gulpGalen.check(options))
+        .pipe(es.writeArray(function (err, arr) {
+          assert(!err, "There where errors present");
+          assert(arr, "Result missing");
+          assert(arr.length === 2, "False number of files: " + util.inspect(arr));
+          for (var i = 0; i < 2; i++) {
+            assert(arr[i].path.match(/specs\/google.\.gspec$/), "File's path didn't end with specs/google?.gspec: '" + arr[i].path + "'");
+          }
           done();
         }));
+    });
+
+    it("should handle failed specs", function (done) {
+      gulp.src("**/specs/google_failing.gspec")
+        .pipe(gulpGalen
+          .check(options)
+          .on("error", function () {
+            done();
+          }));
+    });
+
   });
 
-});
+  describe("extended functionality", function () {
 
-describe("gulp-galen's extended functionality", function () {
+    this.timeout(30000);
 
-  this.timeout(30000);
+    before(function (done) {
+      rimraf("./tmp/test-reports", done);
+    });
 
-  before(function(done) {
-    rimraf("./tmp/test-reports", done);
-  });
+    it("should support some variables based upcon the current file", function (done) {
+      gulp.src("**/specs/google1.gspec")
+        .pipe(gulpGalen.check({
+          url: "https://www.google.com",
+          size: "800x600",
+          galenPath: "./node_modules/.bin/galen",
+          testngreport: "./tmp/test-reports/testng-{basename}.xml"
+        }))
+        .pipe(es.writeArray(function (err, arr) {
+          var fn = "./tmp/test-reports/testng-google1.gspec.xml";
+          fs.stat(fn, function (err, stats) {
+            assert(!err, "File not found: " + fn);
+            assert(stats.isFile(), "Is no file: " + fn);
+            done();
+          });
+        }));
+    });
 
-  it("should support some variables based upcon the current file", function (done) {
-    gulp.src("**/specs/google1.gspec")
-      .pipe(gulpGalen.check({
-        url: "https://www.google.com",
-        size: "800x600",
-        galenPath: "./node_modules/.bin/galen",
-        testngreport: "./tmp/test-reports/testng-{basename}.xml"
-      }))
-      .pipe(es.writeArray(function (err, arr) {
-        var fn = "./tmp/test-reports/testng-google1.gspec.xml";
-        fs.stat(fn, function(err, stats) {
-          assert(!err, "File not found: " + fn);
-          assert(stats.isFile(), "Is no file: " + fn);
-          done();
-        });
-      }));
   });
 
 });
